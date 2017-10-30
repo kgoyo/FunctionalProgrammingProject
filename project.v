82a1331cbdc4d2a287b993d430d23bf8b1012584
Andreas Østergaard Nielsen <kgoyo666@gmail.com>
2017-10-30 12:09:52 +0100

    implements insert (with tests)
    has start on search that does not work

---
+++ project.v
@@ -0,0 +1,89 @@
+Inductive tree23 : Type :=
+  | empty : tree23
+  | node2 : nat -> tree23 -> tree23 -> tree23
+  | node3 : nat -> nat -> tree23 -> tree23 -> tree23 -> tree23
+.
+
+Fixpoint search23tree (sk:nat) (n:tree23) : bool :=
+  match n with
+  | empty => false
+  | node2 k n1 n2 => 
+    match Nat.compare sk k with
+    | Eq => true
+    | Lt => search23tree sk n1
+    | Gt => search23tree sk n2
+    end
+  | node3 k1 k2 n1 n2 n3 =>
+    match Nat.compare sk k1 , Nat.compare sk k2 with
+    | Eq,_ => true
+    | _,Eq => true
+    | Lt,_ => search23tree sk n1
+    | _,Gt => search23tree sk n3
+    | _,_ => search23tree sk n2
+    end
+  end.
+
+Example searchtest1: search23tree 2 empty = false.
+Proof. reflexivity. Qed.
+
+Example searchtest2: search23tree 2 (node2 2 empty empty) = true.
+Proof. reflexivity. Qed.
+
+Example searchtest3: search23tree 1 (node2 2 empty empty) = false.
+Proof. reflexivity. Qed.
+
+Example searchtest4: search23tree 3 (node2 2 empty empty) = false.
+Proof. reflexivity. Qed.
+
+Example searchtest5: search23tree 2 (node3 2 4 empty empty empty) = true.
+Proof. reflexivity. Qed.
+
+Example searchtest6: search23tree 4 (node3 2 4 empty empty empty) = true.
+Proof. reflexivity. Qed.
+
+Example searchtest7: search23tree 1 (node3 2 4 empty empty empty) = false.
+Proof. reflexivity. Qed.
+
+Example searchtest8: search23tree 3 (node3 2 4 empty empty empty) = false.
+Proof. reflexivity. Qed.
+
+Example searchtest9: search23tree 5 (node3 2 4 empty empty empty) = false.
+Proof. reflexivity. Qed.
+
+inductive
+
+Fixpoint insert23tree (k:nat) (t:tree23) : tree23 :=
+  | empty => node2 k empty empty
+  | node2 k1 empty empty =>
+    match Nat.leb k k1 with
+    | true => node3 k k1 empty empty empty
+    | false => node3 k1 k empty empty empty
+    end
+  | node2 k1 t1 t2 =>
+    match Nat.leb k k1 with
+    | true => 
+      match (insert23tree k t1) with
+    | false => 
+    end
+
+(*
+vi kunne lave en ny type illegal tree som har en node4, som derefter skal blive legalized
+vi kan så match når noget returnerer en node4 og så splitte den op
+*)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
